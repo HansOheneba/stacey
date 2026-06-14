45ff784d853c1d191d7e3dcdb68facd23c3d9066
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2026-06-14 15:39:13 +0000

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,33 @@ export const metadata: Metadata = {
   title: "Stacey Elsie Lamptey | Speaker & Growth Strategist",
   description:
     "Stacey Elsie Lamptey is a speaker and growth strategist focused on identity, faith-based growth, and mindset development.",
+  icons: {
+    icon: [
+      { url: "/favicon/favicon.ico", sizes: "any" },
+      {
+        url: "/favicon/favicon-16x16.png",
+        type: "image/png",
+        sizes: "16x16",
+      },
+      {
+        url: "/favicon/favicon-32x32.png",
+        type: "image/png",
+        sizes: "32x32",
+      },
+      {
+        url: "/favicon/android-chrome-192x192.png",
+        type: "image/png",
+        sizes: "192x192",
+      },
+      {
+        url: "/favicon/android-chrome-512x512.png",
+        type: "image/png",
+        sizes: "512x512",
+      },
+    ],
+    apple: [{ url: "/favicon/apple-touch-icon.png", sizes: "180x180" }],
+  },
+  manifest: "/favicon/site.webmanifest",
 };
 
 export default function RootLayout({
